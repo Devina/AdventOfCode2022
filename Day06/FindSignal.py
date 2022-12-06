02619868244442e3e5c2d3f4c88688b5b2d1a152
Devina <36676408+Devina@users.noreply.github.com>
2022-12-06 07:21:46 +0000

    Add Day 6 Code

---
+++ Day06/FindSignal.py
@@ -0,0 +1,19 @@
+with open('Datastream.txt', 'r') as f:
+    content = f.readlines()
+
+# Define variables
+count = 0
+first_start_packet_marker = 0
+
+# Loop through each line in the file
+for line in content:
+    for c in line:  # Accounting for start character
+        has_repeated_chars = len(set(line[count:count+4:1])) != len(line[count:count+4:1])  # If repeated characters in the extracted string then is True else False
+        if has_repeated_chars:
+            count+=1
+        else:
+            break
+
+first_start_packet_marker = count + 4 # Counter account for point upto when repeated characters. So first instance is distinct characters after.
+
+print("Characters to be processed before the first start-of-message marker is detected: ", first_start_packet_marker)
